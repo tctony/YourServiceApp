aac51f3b5d2f0c75110f8dfb87fa42ecb32395f1
Tony Tang <tangchang21@gmail.com>
2016-01-14 23:46:38 +0800

    fetch push token

--- ios/YourService/AppDelegate.swift
+++ ios/YourService/AppDelegate.swift
@@ -15,20 +15,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
 
+        registerNotification(application)
+
         var jsCodeLocation: NSURL?
-        jsCodeLocation = NSURL(string: "http://localhost:8081/index.ios.bundle?platform=ios&dev=true");
+        jsCodeLocation = NSURL(string: "http://192.168.0.104:8081/index.ios.bundle?platform=ios&dev=true");
         //jsCodeLocation = NSBundle.mainBundle().URLForResource("main", withExtension: "jsbundle");
 
         let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "YourService", initialProperties: nil, launchOptions: launchOptions)
 
-        self.window = UIWindow.init(frame: UIScreen.mainScreen().bounds)
-        let rootViewController = UIViewController();
-        rootViewController.view = rootView;
-        self.window!.rootViewController = rootViewController;
-        self.window!.makeKeyAndVisible()
+        window = UIWindow.init(frame: UIScreen.mainScreen().bounds)
+        let rootViewController = UIViewController()
+        rootViewController.view = rootView
+        window!.rootViewController = rootViewController
+        window!.makeKeyAndVisible()
         
         return true
     }
+
+    func registerNotification(application: UIApplication) {
+        let types: UIUserNotificationType = [ .Badge, .Sound, .Alert ]
+        let settings = UIUserNotificationSettings(forTypes: types, categories: nil)
+        application.registerUserNotificationSettings(settings)
+        application.registerForRemoteNotifications()
+    }
     
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        let token = deviceToken.description
+            .stringByReplacingOccurrencesOfString("<", withString: "")
+            .stringByReplacingOccurrencesOfString(" ", withString: "")
+            .stringByReplacingOccurrencesOfString(">", withString: "")
+        print("got push token:", token)
+    }
     
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print("get token failed:", error)
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        print("get push with userinfo:", userInfo)
+    }
 }
